9701e5de93a54621d6f60f7e56b6e8688062ea95
ARGProject-bit <65083215+ARGProject-bit@users.noreply.github.com>
2021-10-07 19:44:18 +0200

    alvrodgar2 en pom + owner de mascota

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -44,7 +44,7 @@ INSERT INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Mad
 INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487', 'owner1');
 INSERT INTO owners VALUES (11, 'Luis', 'Chacon', '06011 Valdepasillas', 'Badajoz', '6085555487', 'owner1');
 INSERT INTO owners VALUES (12, 'Jose', 'Martin', 'Calle Ejemplo 22', 'Sevilla', '6085555487', 'owner1');
-
+INSERT INTO owners VALUES (13, 'Alvaro', 'Rodriguez', 'Calle Otro Ejemplo 42', 'Sevilla', '6085555489', 'owner1');
 
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (1, 'Leo', '2010-09-07', 1, 1);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (2, 'Basil', '2012-08-06', 6, 2);
@@ -60,6 +60,7 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (11, 'Freddy', '201
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Perro', '2012-07-08', 1, 11);
+INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (15, 'Fuwa', '2019-02-03', 1, 13);
 
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02', 'rabies shot');
