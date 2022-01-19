42d2d8f034559eac01b13a8ba6b78b0448e62f16
eloy <eloy@DESKTOP-S59S3J4>
2022-01-19 23:32:37 +0100

    cambio menor

--- src/test/java/org/springframework/samples/petclinic/jugador/JugadorServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/jugador/JugadorServiceTests.java
@@ -5,28 +5,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.samples.petclinic.game.BoardService;
+
 import org.springframework.samples.petclinic.game.Game;
-import org.springframework.samples.petclinic.game.GameService;
+
 import org.springframework.samples.petclinic.persona.Persona;
-import org.springframework.samples.petclinic.persona.PersonaService;
+import org.springframework.samples.petclinic.persona.PersonaRepository;
+
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 public class JugadorServiceTests {
 
-	@Autowired
-	private GameService gameService;
-	@Autowired
-	private BoardService boardService;
+	
 	@Autowired
 	private JugadorService jugadorService;
-	private PersonaService personaService;
+	@Autowired
+	private PersonaRepository personaRepo;
 
 	@Test
 	public void historialgameTest() {
@@ -34,7 +33,7 @@ public class JugadorServiceTests {
 		
 		Persona persona = personaRepo.findById(2).get();	
 		List<Game> list = jugadorService.historialgame(persona);
-		assertEquals(list.size(), 1);
+		assertEquals(list.size(), 2);
 	}
 
 	
