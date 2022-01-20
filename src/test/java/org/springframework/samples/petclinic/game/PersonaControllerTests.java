a900542e331fc9c8f8bdf8cf335d34f138915f10
Juan Antonio <juanantoniomenavargas@gmail.com>
2022-01-20 22:51:49 +0100

    Pruebas de Clase PersonaControllerTest y GameControllerTest

--- src/test/java/org/springframework/samples/petclinic/game/PersonaControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/game/PersonaControllerTests.java
@@ -5,9 +5,12 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,17 +20,19 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
 import org.springframework.samples.petclinic.jugador.JugadorService;
-import org.springframework.samples.petclinic.owner.Owner;
 import org.springframework.samples.petclinic.persona.Persona;
+import org.springframework.samples.petclinic.persona.PersonaController;
 import org.springframework.samples.petclinic.persona.PersonaService;
+import org.springframework.samples.petclinic.user.UserService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.samples.petclinic.user.AuthoritiesService;
+import org.springframework.samples.petclinic.user.User;
+@WebMvcTest(value = PersonaController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 public class PersonaControllerTests {
+	 private static final String FORM = "personas/createOrUpdatePersonaForm";
 
-
-		private static final int TEST_PERSONA_ID = 1;
 		@MockBean
 		private GameService gameService;
 
@@ -42,21 +47,38 @@ public class PersonaControllerTests {
 
 		@MockBean
 		private PersonaService personaService;
+		@MockBean
+		private UserService userService;
+		@MockBean
+		private AuthoritiesService authoritiesService;
 
-		private Game game;
 
 		@Autowired
 		private MockMvc mockMvc;
 		
-		private Persona prueba;
+		private Persona persona;
+		private Persona persona2;
 
 		@BeforeEach
 		void setUp() {
-			prueba = new Persona();
-			prueba.setId(TEST_PERSONA_ID);
-			prueba.setFirstName("George");
-			prueba.setLastName("Franklin");
-			//prueba.setUserName("prueba1");
+			persona = new Persona();
+			User u= new User();
+			u.setUsername("person3");
+			u.setPassword("personi");
+			persona.setId(4);
+			persona.setFirstName("George");
+			persona.setLastName("Franklin");
+			persona.setUser(u);
+			
+			
+			List<Persona> result =  new ArrayList<Persona>();
+			result.add(persona);
+			result.add(persona2);
+			
+			Iterable<Persona> it = result;
+			
+			
+			given(this.personaService.findId(4)).willReturn(persona);
 
 			
 		}
@@ -65,8 +87,69 @@ public class PersonaControllerTests {
 //		@Test
 //		void testGamePerson() throws Exception {
 //			mockMvc.perform(get("/personas"))
-//			.andExpect(model().attribute("prueba", hasProperty("lastName", is("Franklin"))))
-//			.andExpect(model().attribute("prueba", hasProperty("firstName", is("George"))))
+//			.andExpect(status().isOk());
+//			.andExpect(model().attributeExists("games"))
 //			.andExpect(view().name("personas/partidaspersona"));
 //		}
+		
+		@WithMockUser(value = "persona")
+		@Test
+		void testGamePersonaLista() throws Exception {
+			mockMvc.perform(get("/personas/registro"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("personas"))
+			.andExpect(view().name("personas/listPersonas"));
+		}
+		
+		@WithMockUser(value = "persona")
+		@Test
+		void testinitCreationForm() throws Exception {
+			mockMvc.perform(get("/register"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("persona"))
+			.andExpect(view().name(FORM));
+		}
+		
+		@WithMockUser(value = "persona")
+		@Test
+		void testProcessCreationErrorForm() throws Exception {
+			mockMvc.perform(post("/register")
+			
+			.param("firstName","George")
+			.with(csrf())
+			.param("lastName", "Franklin")
+			.param("username", "person3")
+			.param("password", "personi"))
+			.andExpect(view().name("redirect:/"));
+		}
+		
+		@WithMockUser(value = "persona")
+		@Test
+		void testInitEditPersona() throws Exception {
+			mockMvc.perform(get("/personas/edit/{personaId}",4))
+					.andExpect(status().isOk())
+					.andExpect(model().attribute("persona", hasProperty("id", is(4))))
+					.andExpect(model().attribute("persona", hasProperty("firstName", is("George"))))
+					.andExpect(model().attribute("persona", hasProperty("lastName", is("Franklin"))))
+					.andExpect(view().name(FORM));
+				
+			
+		}
+		
+		@WithMockUser(value = "persona")
+		@Test
+		void testProccesEditPersona() throws Exception {
+			mockMvc.perform(post("/personas/edit/{personaId}",4)
+					.with(csrf())
+					.param("firstName","George")
+					.param("lastName", "Franklin")
+					.param("username", "person3")
+					.param("password", "personi"))
+					.andExpect(view().name("redirect:/personas/registro"));
+
+		}
+		
+		
+		
+		
 }
